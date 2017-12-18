e8a67e861b4d53220a3f002f7b77aee3df9243a7
list412 <erestor412@gmail.com>
2017-12-19 01:30:37 +0500

    few buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import jdk.nashorn.internal.ir.annotations.Ignore;
+
 public class Controller {
+
+
+    public Button btn_new;
+    public Button btn_saveAs;
+    public Button btn_save;
+    public Button btn_open;
+    public TextArea txtArea;
+
+    @FXML
+    private void newDoc_bnt_action(ActionEvent event) {
+
+        txtArea.setText("");
+    }
+
+    @FXML
+    public void openDoc_btn_action(ActionEvent event) {
+
+
+
+    }
+
+    public void saveDoc_btn_action(ActionEvent event) {
+
+    }
+
+    public void saveDocAS_btn_action(ActionEvent event) {
+    }
 }
