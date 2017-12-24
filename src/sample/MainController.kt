36bdaa96a88e707c7b89cc158fe4fdb6d651d1fa
list412 <erestor412@gmail.com>
2017-12-24 07:16:44 +0500

    kotlin update

--- src/sample/MainController.kt
+++ src/sample/MainController.kt
@@ -11,15 +11,28 @@ import javafx.stage.Window
 
 import java.io.File
 import java.io.FileReader
+import java.io.FileWriter
+import java.io.PrintWriter
+import java.nio.file.FileSystem
+import javax.swing.filechooser.FileSystemView
 
 class MainController {
 
-
+    @FXML
     var btn_new: Button? = null
+    @FXML
     var btn_saveAs: Button? = null
+    @FXML
     var btn_save: Button? = null
+    @FXML
     var btn_open: Button? = null
+    @FXML
     var txtArea: TextArea? = null
+
+
+    val opener = FileChooser()
+    var savePath:String?=null
+
 
     private var stage: Stage? = null
 
@@ -35,22 +48,40 @@ class MainController {
 
     @FXML
     fun openDoc_btn_action(event: ActionEvent) {
-
-        val opener = FileChooser()
-
-        opener.initialDirectory = File(System.getProperty("user.home"))
+        if (opener.initialDirectory == null)
+            opener.initialDirectory = FileSystemView.getFileSystemView().homeDirectory
         val file = opener.showOpenDialog(stage!!.owner)
 
         if (file != null) {
+            opener.initialDirectory = file.parentFile
             val reader = FileReader(file)
+            var text = reader.readText()
+            txtArea?.text = text
+            savePath = file.path
+            reader.close();
         }
-
-
     }
 
     fun saveDoc_btn_action(event: ActionEvent) {
-
+        if (savePath != null)
+        {
+           savedoc(savePath)
+        }
     }
 
-    fun saveDocAS_btn_action(event: ActionEvent) {}
+    fun saveDocAS_btn_action(event: ActionEvent) {
+        val file = opener.showSaveDialog(stage!!.owner)
+        if(file != null)
+        {
+            savedoc(file.path)
+        }
+    }
+
+    fun savedoc(path:String?)
+    {
+        val writer = PrintWriter(path)
+        writer.flush()
+        writer.print(txtArea?.text)
+        writer.close()
+    }
 }
